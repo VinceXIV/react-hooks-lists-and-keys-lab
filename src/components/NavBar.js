dcba37fb19c2f101b5047300abc898ec8aa79328
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-22 20:39:49 +0300

    Complete Navbar Deliverable

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkObjects = links.map(link => {
+    return {id: uuid(), link: link}
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const domLinks = linkObjects.map(item => {
+    const itemName = item.link
+    return <a href={"#"+{itemName}} key={item.id}>{item.link}</a>
+  })
+
+  console.log("domlinks: ", domLinks)
+
+  return <nav>{domLinks}</nav>;
 }
 
 export default NavBar;
